af8c45de31bd270cd25f6075cf96443eecbc9f67
Vineet John <iamvineetjohn@gmail.com>
2018-02-25 12:47:58 -0500

    Generify run script project and execution paths

--- run_authorship_style_transfer_network.sh
+++ run_authorship_style_transfer_network.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
-export PYTHONPATH=$HOME/projects/authorship-style-transfer/
+source $HOME/.pyenv/bin/activate
+
+PROJECT_DIR_PATH="$PWD/$(dirname $0)/"
+echo $PROJECT_DIR_PATH
+cd $PROJECT_DIR_PATH
+
+export PYTHONPATH=$PROJECT_DIR_PATH
 export CUDA_DEVICE_ORDER="PCI_BUS_ID"
 export CUDA_VISIBLE_DEVICES="0"
 
-source $HOME/.pyenv/bin/activate
-
 python -u authorship_style_transfer_network/main.py
+
