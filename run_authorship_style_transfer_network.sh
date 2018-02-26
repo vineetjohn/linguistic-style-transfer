2dece31efdc3e8928b0bc0fcbda599c6f6c17db8
Vineet John <iamvineetjohn@gmail.com>
2018-02-26 14:48:05 -0500

    Modularize code; Decouple training and inference runs by saving and restoring model

--- run_authorship_style_transfer_network.sh
+++ run_authorship_style_transfer_network.sh
@@ -6,8 +6,7 @@ PROJECT_DIR_PATH="$PWD/$(dirname $0)/"
 cd $PROJECT_DIR_PATH
 
 export PYTHONPATH=$PROJECT_DIR_PATH
-export CUDA_DEVICE_ORDER="PCI_BUS_ID"
-export CUDA_VISIBLE_DEVICES="0"
+#export CUDA_DEVICE_ORDER="PCI_BUS_ID"
+#export CUDA_VISIBLE_DEVICES="0"
 
-python -u authorship_style_transfer_network/main.py
-
+python -u authorship_style_transfer_network/main.py "$@"
