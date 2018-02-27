8b326f91a1176a53f163da6365c63f36c872335e
Vineet John <iamvineetjohn@gmail.com>
2018-02-27 14:50:43 -0500

    Add UNK tokens and change digit placeholder; Change target sequence length for the decoder

--- authorship_style_transfer_network/tests/tensor_slicing.py
+++ authorship_style_transfer_network/tests/tensor_slicing.py
@@ -14,5 +14,21 @@ with tf.Session() as sess:
                 [7, 8, 9]
             ]
         ])
-    print(foo)
-    print(sess.run(tf.slice(foo, [0, 0, 0], [foo.shape[0], foo.shape[1], 1])))
+
+    bar = tf.constant(
+        [
+            [1, 2, 3],
+            [4, 5, 6]
+        ])
+
+    bar_1 = tf.add([1, 2, 3], 1)
+    # baz = tf.fill(dims=[bar.shape[0]], value=7)
+    # baz = tf.tile(input=tf.constant([[7]]), multiples=[2, 1])
+
+    # print(foo)
+    # print(bar)
+    # print(sess.run(bar))
+    print(sess.run(bar_1))
+    # print(sess.run(baz))
+    # print(sess.run(tf.concat([baz, bar], axis=1)))
+    # print(sess.run(tf.strided_slice(foo, [0, 0], [2, -1], [1, 1], name='slice_input')))
--- authorship_style_transfer_network/utils/data_preprocessor.py
+++ authorship_style_transfer_network/utils/data_preprocessor.py
@@ -6,24 +6,42 @@ MAX_SEQUENCE_LENGTH = 20
 
 def get_text_sequences(text_file_path, vocab_size):
 
-    text_tokenizer = tf.keras.preprocessing.text.Tokenizer(
-        num_words=vocab_size, filters='!"#$%&()*+,-./:;=?@[\\]^_`{|}~\t\n')
+    word_index = {
+        'pad': 0,
+        'sos': 1,
+        'eos': 2,
+        'unk': 3
+    }
+
+    text_tokenizer = tf.keras.preprocessing.text.Tokenizer()
 
     with open(text_file_path) as text_file:
         text_tokenizer.fit_on_texts(text_file)
+
+    num_predefined_tokens = len(word_index)
+    for index, word in enumerate(text_tokenizer.word_index):
+        word_index[word] = index + num_predefined_tokens
+    text_tokenizer.word_index = word_index
 
     with open(text_file_path) as text_file:
         integer_text_sequences = text_tokenizer.texts_to_sequences(text_file)
 
     text_sequence_lengths = np.asarray(
-        a=list(map(lambda x: len(x), integer_text_sequences)), dtype=np.int32)
+        a=list(map(lambda x: len(x), integer_text_sequences)),
+        dtype=np.int32)
 
     # max_sequence_length = MAX_SEQUENCE_LENGTH
     max_sequence_length = int(np.median(text_sequence_lengths))
     print("max_sequence_length: ", max_sequence_length)
 
+    def mark_unknown_words(integer_text_sequence):
+        return list(map(lambda x: x if x < vocab_size else word_index['unk'], integer_text_sequence))
+    integer_text_sequences = list(map(mark_unknown_words, integer_text_sequences))
+    integer_text_sequences = list(map(lambda x: x + [word_index['eos']], integer_text_sequences))
+
     padded_sequences = tf.keras.preprocessing.sequence.pad_sequences(
-        integer_text_sequences, maxlen=max_sequence_length, padding='post', truncating='post')
+        integer_text_sequences, maxlen=max_sequence_length, padding='post', truncating='post',
+        value=word_index['pad'])
 
     text_sequence_lengths = np.asarray(list(map(
         lambda x: max_sequence_length if x > max_sequence_length else x, text_sequence_lengths)))
@@ -34,7 +52,7 @@ def get_text_sequences(text_file_path, vocab_size):
 
 def get_labels(label_file_path):
 
-    label_tokenizer = tf.keras.preprocessing.text.Tokenizer(lower=False)
+    label_tokenizer = tf.keras.preprocessing.text.Tokenizer()
 
     with open(label_file_path) as label_file:
         label_tokenizer.fit_on_texts(label_file)
