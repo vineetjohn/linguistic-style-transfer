d72518e32c5623d6e2eb39a8dd962c83c0bf5df6
Vineet John <iamvineetjohn@gmail.com>
2018-03-01 10:54:45 -0500

    Fix bug for inability to check for out-of-vocab token embeddings

--- authorship_style_transfer_network/utils/word_embedder.py
+++ authorship_style_transfer_network/utils/word_embedder.py
@@ -1,8 +1,9 @@
 import gensim
+import numpy as np
 
 
 def get_word2vec_embedding(word, model):
-    vec_rep = None
+    vec_rep = np.asarray([])
     if word in model:
         vec_rep = model[word]
 
@@ -19,7 +20,7 @@ def add_word_vectors_to_embeddings(word_index, word_vector_path, encoder_embeddi
     i = 0
     for key in word_index:
         word_embedding = get_word2vec_embedding(key, wv_model)
-        if word_embedding:
+        if word_embedding.any():
             encoder_embedding_matrix[i] = word_embedding
             decoder_embedding_matrix[i] = word_embedding
         i += 1
