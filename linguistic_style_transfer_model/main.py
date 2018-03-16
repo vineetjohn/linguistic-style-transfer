Fix inclusion of unnecessary tokens for BLEU score computation; Clean up function parameters

linguistic_style_transfer_model/main.py
@@ -11,25 +11,23 @@ from linguistic_style_transfer_model.config import global_config, model_config
 from linguistic_style_transfer_model.models import adversarial_autoencoder
 from linguistic_style_transfer_model.utils import bleu_scorer, data_postprocessor, \
     data_preprocessor, log_initializer, word_embedder
+from linguistic_style_transfer_model.config.options import Options
 
 logger = None
 
 
-def get_data(text_file_path, vocab_size, label_file_path):
-    padded_sequences, text_sequence_lengths, word_index, max_sequence_length, actual_sequences = \
-        data_preprocessor.get_text_sequences(text_file_path, vocab_size)
+def get_data(options):
+    [word_index, actual_sequences, padded_sequences, text_sequence_lengths] = \
+        data_preprocessor.get_text_sequences(options.text_file_path, options.vocab_size)
     logger.debug("text_sequence_lengths: {}".format(text_sequence_lengths.shape))
     logger.debug("padded_sequences: {}".format(padded_sequences.shape))
 
-    sos_index = word_index[global_config.sos_token]
-    eos_index = word_index[global_config.eos_token]
-    data_size = padded_sequences.shape[0]
-
-    one_hot_labels, num_labels, label_sequences = data_preprocessor.get_labels(label_file_path)
+    label_sequences, one_hot_labels, num_labels = \
+        data_preprocessor.get_labels(options.label_file_path)
     logger.debug("one_hot_labels.shape: {}".format(one_hot_labels.shape))
 
-    return num_labels, max_sequence_length, vocab_size, sos_index, eos_index, padded_sequences, \
-           one_hot_labels, text_sequence_lengths, label_sequences, data_size, word_index, actual_sequences
+    return [word_index, actual_sequences, padded_sequences, text_sequence_lengths,
+            label_sequences, one_hot_labels, num_labels]
 
 
 def get_average_label_embeddings(data_size, label_sequences):
@@ -56,13 +54,9 @@ def get_average_label_embeddings(data_size, label_sequences):
     return average_label_embeddings
 
 
-def flush_ground_truth_sentences(actual_sequences, start_index, final_index, max_sequence_length,
-                                 word_index, inverse_word_index, timestamped_file_suffix):
+def flush_ground_truth_sentences(actual_sequences, start_index, final_index,
+                                 inverse_word_index, timestamped_file_suffix):
     actual_sequences = actual_sequences[start_index:final_index]
-
-    actual_sequences = tf.keras.preprocessing.sequence.pad_sequences(
-        actual_sequences, maxlen=max_sequence_length, padding='post', truncating='post',
-        value=word_index[global_config.eos_token])
 
     actual_word_lists = \
         [data_postprocessor.generate_words_from_indices(x, inverse_word_index)
@@ -78,20 +72,15 @@ def flush_ground_truth_sentences(actual_sequences, start_index, final_index, max
     return actual_word_lists
 
 
-def execute_post_inference_operations(word_index, actual_word_lists, generated_sequences,
-                                      final_sequence_lengths, max_sequence_length,
+def execute_post_inference_operations(actual_word_lists, generated_sequences, final_sequence_lengths,
                                       inverse_word_index, timestamped_file_suffix, mode):
     logger.debug("Minimum generated sentence length: {}".format(min(final_sequence_lengths)))
 
-    trimmed_generated_sequences = [x[:y] for (x, y) in zip(generated_sequences, final_sequence_lengths)]
-
-    generated_sequences = tf.keras.preprocessing.sequence.pad_sequences(
-        trimmed_generated_sequences, maxlen=max_sequence_length, padding='post', truncating='post',
-        value=word_index[global_config.eos_token])
-
+    trimmed_generated_sequences = [x[:(y - 1)] for (x, y) in
+                                   zip(generated_sequences, final_sequence_lengths)]
     generated_word_lists = \
         [data_postprocessor.generate_words_from_indices(x, inverse_word_index)
-         for x in generated_sequences]
+         for x in trimmed_generated_sequences]
 
     # Evaluate model scores
     bleu_scores = bleu_scorer.get_corpus_bleu_scores(
@@ -105,20 +94,23 @@ def execute_post_inference_operations(word_index, actual_word_lists, generated_s
             output_file.write(sentence + "\n")
 
 
-def get_word_embeddings(vocab_size, word_index, use_pretrained_embeddings, train_model):
+def get_word_embeddings(word_index, use_pretrained_embeddings, train_model):
     encoder_embedding_matrix = np.random.uniform(
-        low=-0.05, high=0.05, size=(vocab_size, global_config.embedding_size)).astype(dtype=np.float32)
+        size=(global_config.vocab_size, global_config.embedding_size),
+        low=-0.05, high=0.05).astype(dtype=np.float32)
     logger.debug("encoder_embedding_matrix: {}".format(encoder_embedding_matrix.shape))
 
     decoder_embedding_matrix = np.random.uniform(
-        low=-0.05, high=0.05, size=(vocab_size, global_config.embedding_size)).astype(dtype=np.float32)
+        size=(global_config.vocab_size, global_config.embedding_size),
+        low=-0.05, high=0.05).astype(dtype=np.float32)
     logger.debug("decoder_embedding_matrix: {}".format(decoder_embedding_matrix.shape))
 
     if train_model and use_pretrained_embeddings:
         logger.info("Loading pretrained embeddings")
-        encoder_embedding_matrix, decoder_embedding_matrix = word_embedder.add_word_vectors_to_embeddings(
-            word_index, global_config.word_vector_path, encoder_embedding_matrix,
-            decoder_embedding_matrix, vocab_size)
+        encoder_embedding_matrix, decoder_embedding_matrix = \
+            word_embedder.add_word_vectors_to_embeddings(
+                word_index, global_config.word_vector_path, encoder_embedding_matrix,
+                decoder_embedding_matrix)
 
     return encoder_embedding_matrix, decoder_embedding_matrix
 
@@ -130,52 +122,48 @@ def main(argv):
     parser.add_argument("--infer-sequences", action="store_true", default=False)
     parser.add_argument("--generate-novel-text", action="store_true", default=False)
     parser.add_argument("--use-pretrained-embeddings", action="store_true", default=False)
-    parser.add_argument("--training-epochs", type=int, default=10)
-    parser.add_argument("--vocab-size", type=int, default=1000)
     parser.add_argument("--text-file-path", type=str, required=True)
     parser.add_argument("--label-file-path", type=str, required=True)
+    parser.add_argument("--vocab-size", type=int, default=1000)
+    parser.add_argument("--training-epochs", type=int, default=10)
     parser.add_argument("--logging-level", type=str, default="INFO")
 
-    args_namespace = parser.parse_args(argv)
-    command_line_args = vars(args_namespace)
+    options = parser.parse_args(args=argv, namespace=Options())
 
     global logger
-    logger = log_initializer.setup_custom_logger(
-        global_config.logger_name, command_line_args['logging_level'])
+    logger = log_initializer.setup_custom_logger(global_config.logger_name, options.logging_level)
 
-    if not (command_line_args['train_model'] or command_line_args['infer_sequences'] or
-            command_line_args['generate_novel_text']):
+    if not (options.train_model or options.infer_sequences or options.generate_novel_text):
         logger.info("Nothing to do. Exiting ...")
         sys.exit(0)
 
+    global_config.training_epochs = options.training_epochs
+
     # Retrieve all data
     logger.info("Reading data ...")
-    num_labels, max_sequence_length, vocab_size, sos_index, eos_index, padded_sequences, \
-    one_hot_labels, text_sequence_lengths, label_sequences, data_size, word_index, actual_sequences = \
-        get_data(command_line_args['text_file_path'], command_line_args['vocab_size'],
-                 command_line_args['label_file_path'])
+    [word_index, actual_sequences, padded_sequences, text_sequence_lengths,
+     label_sequences, one_hot_labels, num_labels] = get_data(options)
+    data_size = padded_sequences.shape[0]
 
     encoder_embedding_matrix, decoder_embedding_matrix = \
-        get_word_embeddings(vocab_size, word_index, command_line_args['use_pretrained_embeddings'],
-                            command_line_args['train_model'])
+        get_word_embeddings(word_index, options.use_pretrained_embeddings, options.train_model)
 
     # Build model
     logger.info("Building model architecture ...")
     network = adversarial_autoencoder.AdversarialAutoencoder(
-        num_labels, max_sequence_length, vocab_size, sos_index, eos_index,
-        encoder_embedding_matrix, decoder_embedding_matrix, padded_sequences,
-        one_hot_labels, text_sequence_lengths, label_sequences)
+        padded_sequences, text_sequence_lengths, one_hot_labels, num_labels,
+        word_index, encoder_embedding_matrix, decoder_embedding_matrix)
     network.build_model()
 
     # Train and save model
-    if command_line_args['train_model']:
+    if options.train_model:
         logger.info("Training model ...")
         sess = get_tensorflow_session()
-        network.train(sess, data_size, command_line_args['training_epochs'])
+        network.train(sess, data_size)
         sess.close()
         logger.info("Training complete!")
 
-    if command_line_args['infer_sequences'] or command_line_args['generate_novel_text']:
+    if options.infer_sequences or options.generate_novel_text:
         samples_size = data_size - (data_size % model_config.batch_size)
         offset = 0
         logger.debug("Sampling range: {}-{}".format(offset, (offset + samples_size)))
@@ -184,36 +172,36 @@ def main(argv):
         timestamped_file_suffix = dt.now().strftime("%Y%m%d%H%M%S")
 
         actual_word_lists = flush_ground_truth_sentences(
-            actual_sequences, offset, offset + samples_size, max_sequence_length,
-            word_index, inverse_word_index, timestamped_file_suffix)
+            actual_sequences, offset, offset + samples_size,
+            inverse_word_index, timestamped_file_suffix)
 
         # Restore model and run inference
-        if command_line_args['infer_sequences']:
+        if options.infer_sequences:
             logger.info("Inferring test samples ...")
             sess = get_tensorflow_session()
             generated_sequences, final_sequence_lengths = \
                 network.infer(sess, offset, samples_size)
             sess.close()
             execute_post_inference_operations(
-                word_index, actual_word_lists, generated_sequences, final_sequence_lengths,
-                max_sequence_length, inverse_word_index, timestamped_file_suffix,
+                actual_word_lists, generated_sequences, final_sequence_lengths,
+                inverse_word_index, timestamped_file_suffix,
                 "reconstructed_sentences")
             logger.info("Inference complete!")
 
         # Enforce a particular style embedding and regenerate text
-        if command_line_args['generate_novel_text']:
+        if options.generate_novel_text:
             logger.info("Generating novel text ...")
             random_style_choice = randint(1, num_labels)
-            logger.debug("Style chosen: {}".format(random_style_choice))
+            logger.info("Style chosen: {}".format(random_style_choice))
             average_label_embeddings = get_average_label_embeddings(
                 data_size, label_sequences)
             style_embedding = np.asarray(average_label_embeddings[random_style_choice])
             sess = get_tensorflow_session()
-            generated_sequences, final_sequence_lengths =\
+            generated_sequences, final_sequence_lengths = \
                 network.generate_novel_sentences(sess, offset, samples_size, style_embedding)
             execute_post_inference_operations(
-                word_index, actual_word_lists, generated_sequences, final_sequence_lengths,
-                max_sequence_length, inverse_word_index, timestamped_file_suffix,
+                actual_word_lists, generated_sequences, final_sequence_lengths,
+                inverse_word_index, timestamped_file_suffix,
                 "novel_sentences")
             logger.info("Generation complete!")
 

linguistic_style_transfer_model/models/adversarial_autoencoder.py
@@ -12,20 +12,15 @@ logger = logging.getLogger(global_config.logger_name)
 
 class AdversarialAutoencoder:
 
-    def __init__(self, num_labels, max_sequence_length, vocab_size, sos_index, eos_index,
-                 encoder_embedding_matrix, decoder_embedding_matrix, padded_sequences, one_hot_labels,
-                 text_sequence_lengths, label_sequences):
+    def __init__(self, padded_sequences, text_sequence_lengths, one_hot_labels, num_labels,
+                 word_index, encoder_embedding_matrix, decoder_embedding_matrix):
+        self.padded_sequences = padded_sequences
+        self.text_sequence_lengths = text_sequence_lengths
+        self.one_hot_labels = one_hot_labels
         self.num_labels = num_labels
-        self.label_sequences = label_sequences
-        self.max_sequence_length = max_sequence_length
-        self.vocab_size = vocab_size
-        self.sos_index = sos_index
-        self.eos_index = eos_index
+        self.word_index = word_index
         self.encoder_embedding_matrix = encoder_embedding_matrix
         self.decoder_embedding_matrix = decoder_embedding_matrix
-        self.padded_sequences = padded_sequences
-        self.one_hot_labels = one_hot_labels
-        self.text_sequence_lengths = text_sequence_lengths
 
     def get_sentence_embedding(self, encoder_embedded_sequence):
         scope_name = "sentence_embedding"
@@ -87,7 +82,7 @@ class AdversarialAutoencoder:
             output_keep_prob=model_config.recurrent_state_keep_prob,
             state_keep_prob=model_config.recurrent_state_keep_prob)
 
-        projection_layer = tf.layers.Dense(units=self.vocab_size, use_bias=False)
+        projection_layer = tf.layers.Dense(units=global_config.vocab_size, use_bias=False)
 
         with tf.name_scope("training_decoder"):
             training_helper = tf.contrib.seq2seq.TrainingHelper(
@@ -102,14 +97,16 @@ class AdversarialAutoencoder:
 
             training_decoder_output, _, _ = tf.contrib.seq2seq.dynamic_decode(
                 decoder=training_decoder, impute_finished=True,
-                maximum_iterations=self.max_sequence_length,
+                maximum_iterations=global_config.max_sequence_length,
                 scope="training_decoder")
 
         with tf.name_scope("inference_decoder"):
             inference_decoder = tf.contrib.seq2seq.BeamSearchDecoder(
                 cell=decoder_cell, embedding=decoder_embeddings,
-                start_tokens=tf.fill([model_config.batch_size], self.sos_index),
-                end_token=self.eos_index,
+                start_tokens=tf.fill(
+                    dims=[model_config.batch_size],
+                    value=self.word_index[global_config.sos_token]),
+                end_token=self.word_index[global_config.eos_token],
                 initial_state=tf.contrib.seq2seq.tile_batch(
                     t=generative_embedding, multiplier=model_config.beam_search_width),
                 beam_width=model_config.beam_search_width, output_layer=projection_layer,
@@ -119,7 +116,7 @@ class AdversarialAutoencoder:
 
             inference_decoder_output, _, final_sequence_lengths = tf.contrib.seq2seq.dynamic_decode(
                 decoder=inference_decoder, impute_finished=False,
-                maximum_iterations=self.max_sequence_length,
+                maximum_iterations=global_config.max_sequence_length,
                 scope="inference_decoder")
 
         return training_decoder_output.rnn_output, \
@@ -130,7 +127,7 @@ class AdversarialAutoencoder:
 
         # model inputs
         self.input_sequence = tf.placeholder(
-            dtype=tf.int32, shape=[model_config.batch_size, self.max_sequence_length],
+            dtype=tf.int32, shape=[model_config.batch_size, global_config.max_sequence_length],
             name="input_sequence")
         logger.debug("input_sequence: {}".format(self.input_sequence))
 
@@ -155,8 +152,12 @@ class AdversarialAutoencoder:
         logger.debug("conditioning_embedding: {}".format(self.conditioning_embedding))
 
         decoder_input = tf.concat(
-            values=[tf.fill(dims=[model_config.batch_size, 1], value=self.sos_index, name="start_tokens"),
-                    self.input_sequence],
+            values=[
+                tf.fill(
+                    dims=[model_config.batch_size, 1],
+                    value=self.word_index[global_config.sos_token],
+                    name="start_tokens"),
+                self.input_sequence],
             axis=1, name="decoder_input")
 
         with tf.device('/cpu:0'):
@@ -280,7 +281,7 @@ class AdversarialAutoencoder:
 
         return ops
 
-    def train(self, sess, data_size, training_epochs):
+    def train(self, sess, data_size):
 
         writer = tf.summary.FileWriter(
             logdir="/tmp/tensorflow_logs/" + dt.now().strftime("%Y%m%d-%H%M%S") + "/",
@@ -335,7 +336,7 @@ class AdversarialAutoencoder:
 
         reconstruction_loss, adversarial_loss, all_summaries = (None, None, None)
 
-        for current_epoch in range(1, training_epochs + 1):
+        for current_epoch in range(1, global_config.training_epochs + 1):
 
             all_style_embeddings = list()
             shuffle_indices = np.random.permutation(np.arange(data_size))
@@ -361,7 +362,7 @@ class AdversarialAutoencoder:
                     shuffled_one_hot_labels, shuffled_text_sequence_lengths, None, False)
                 all_style_embeddings.extend(style_embeddings)
 
-            saver.save(sess=sess, save_path=model_config.model_save_path)
+            saver.save(sess=sess, save_path=global_config.model_save_path)
             writer.add_summary(all_summaries, current_epoch)
             writer.flush()
 
@@ -376,7 +377,7 @@ class AdversarialAutoencoder:
 
         sess.run(tf.global_variables_initializer())
         saver = tf.train.Saver()
-        saver.restore(sess=sess, save_path=model_config.model_save_path)
+        saver.restore(sess=sess, save_path=global_config.model_save_path)
 
         generated_sequences = list()
         final_sequence_lengths = list()
@@ -403,7 +404,7 @@ class AdversarialAutoencoder:
 
         sess.run(tf.global_variables_initializer())
         saver = tf.train.Saver()
-        saver.restore(sess=sess, save_path=model_config.model_save_path)
+        saver.restore(sess=sess, save_path=global_config.model_save_path)
 
         generated_sequences = list()
         final_sequence_lengths = list()

linguistic_style_transfer_model/utils/data_preprocessor.py
@@ -33,22 +33,20 @@ def get_text_sequences(text_file_path, vocab_size):
     text_sequence_lengths = np.asarray(
         a=[len(x) for x in actual_sequences], dtype=np.int32)
 
-    actual_sequences = [
+    global_config.vocab_size = vocab_size if len(word_index) > vocab_size else len(word_index)
+    trimmed_sequences = [
         [x if x < vocab_size else word_index[global_config.unk_token] for x in sequence]
         for sequence in actual_sequences]
 
-    max_sequence_length = global_config.max_sequence_length
-
     padded_sequences = tf.keras.preprocessing.sequence.pad_sequences(
-        actual_sequences, maxlen=max_sequence_length, padding='post',
+        trimmed_sequences, maxlen=global_config.max_sequence_length, padding='post',
         truncating='post', value=word_index[global_config.eos_token])
 
     text_sequence_lengths = np.asarray(
-        [max_sequence_length if x >= max_sequence_length
+        [global_config.max_sequence_length if x >= global_config.max_sequence_length
          else x + 1 for x in text_sequence_lengths])  # x + 1 to accomodate a single EOS token
 
-    return [padded_sequences, text_sequence_lengths, text_tokenizer.word_index, max_sequence_length,
-            actual_sequences]
+    return [word_index, actual_sequences, padded_sequences, text_sequence_lengths]
 
 
 def get_labels(label_file_path):
@@ -66,4 +64,4 @@ def get_labels(label_file_path):
     one_hot_labels = np.asarray(
         [np.eye(num_labels, k=x[0])[0] for x in label_sequences])
 
-    return [one_hot_labels, num_labels, label_sequences]
+    return [label_sequences, one_hot_labels, num_labels]