38eea8df62debce441be547820f185d7b1e60e29
Vineet John <iamvineetjohn@gmail.com>
2018-05-12 20:45:07 -0400

    Change declaration of environment variables in shell scripts

--- run_content_preservation_evaluator.sh
+++ run_content_preservation_evaluator.sh
@@ -5,6 +5,5 @@ source $HOME/.pyenv/bin/activate
 PROJECT_DIR_PATH="$PWD/$(dirname $0)/"
 cd ${PROJECT_DIR_PATH}
 
-export PYTHONPATH=${PROJECT_DIR_PATH}
-
+PYTHONPATH=${PROJECT_DIR_PATH} \
 python -u linguistic_style_transfer_model/evaluators/content_preservation.py "$@"
