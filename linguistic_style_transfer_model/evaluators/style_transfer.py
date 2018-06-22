57a3e78f1147cb87469192d3d186e52e7ed54875
Vineet John <iamvineetjohn@gmail.com>
2018-06-22 18:51:21 -0400

    Change training log statement; Close validation session explicitly after each run

--- linguistic_style_transfer_model/evaluators/style_transfer.py
+++ linguistic_style_transfer_model/evaluators/style_transfer.py
@@ -69,6 +69,8 @@ def get_style_transfer_score(classifier_saved_model_path, text_file_path, label)
                 batch_predictions = sess.run(predictions, {input_x: x_test_batch, dropout_keep_prob: 1.0})
                 all_predictions = np.concatenate([all_predictions, batch_predictions])
 
+        sess.close()
+
     # Print accuracy if y_test is defined
     if y_test is not None:
         correct_predictions = float(sum(all_predictions == y_test))
