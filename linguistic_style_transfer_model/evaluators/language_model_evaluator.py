a38ee0af3243147d646bdc43af47f6a87c7137bb
Vineet John <iamvineetjohn@gmail.com>
2018-07-04 12:10:12 -0400

    Make language evaluation script independently runnable

--- linguistic_style_transfer_model/evaluators/language_model_evaluator.py
+++ linguistic_style_transfer_model/evaluators/language_model_evaluator.py
@@ -1,5 +1,13 @@
+import sys
+
+import argparse
 import pickle
 import statistics
+
+from linguistic_style_transfer_model.config import global_config
+from linguistic_style_transfer_model.utils import log_initializer
+
+logger = None
 
 
 def score_generated_sentences(generated_text_file_path, language_model_path):
@@ -11,3 +19,23 @@ def score_generated_sentences(generated_text_file_path, language_model_path):
                 log_probs.append(language_model.score_sent(tuple(sentence.split())))
 
     return statistics.mean(log_probs)
+
+
+def main(argv):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--generated-text-file-path", type=str, required=True)
+    parser.add_argument("--language-model-path", type=str, required=True)
+    args_namespace = parser.parse_args(argv)
+    command_line_args = vars(args_namespace)
+
+    global logger
+    logger = log_initializer.setup_custom_logger(global_config.logger_name, "INFO")
+
+    ll_score = \
+        score_generated_sentences(command_line_args['generated_text_file_path'],
+                                  command_line_args['language_model_path'])
+    logger.info("ll_score: {}".format(ll_score))
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
