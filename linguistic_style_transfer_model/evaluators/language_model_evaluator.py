bec9a882875f86b032cb82796c34fb52be3767f9
Vineet John <iamvineetjohn@gmail.com>
2018-07-07 11:46:55 -0400

    Add support for language model evaluation with kenlm

--- linguistic_style_transfer_model/evaluators/language_model_evaluator.py
+++ linguistic_style_transfer_model/evaluators/language_model_evaluator.py
@@ -3,6 +3,7 @@ import sys
 import argparse
 import pickle
 import statistics
+from types import SimpleNamespace
 
 from linguistic_style_transfer_model.config import global_config
 from linguistic_style_transfer_model.utils import log_initializer
@@ -10,30 +11,47 @@ from linguistic_style_transfer_model.utils import log_initializer
 logger = None
 
 
-def score_generated_sentences(generated_text_file_path, language_model_path):
+class Options(SimpleNamespace):
+
+    def __init__(self):
+        self.generated_text_file_path = "INFO"
+        self.language_model_path = None
+        self.use_kenlm = None
+
+
+def score_generated_sentences(generated_text_file_path, language_model_path, use_kenlm):
     log_probs = list()
-    with open(language_model_path, 'rb') as language_model_file:
-        language_model = pickle.load(language_model_file)
+
+    if use_kenlm:
+        import kenlm
+        model = kenlm.LanguageModel(language_model_path)
         with open(generated_text_file_path) as generated_text_file:
             for sentence in generated_text_file:
-                log_probs.append(language_model.score_sent(tuple(sentence.split())))
+                log_probs.append(model.score(sentence))
+    else:
+        with open(language_model_path, 'rb') as language_model_file:
+            language_model = pickle.load(language_model_file)
+            with open(generated_text_file_path) as generated_text_file:
+                for sentence in generated_text_file:
+                    log_probs.append(language_model.score_sent(tuple(sentence.split())))
 
     return statistics.mean(log_probs)
 
 
 def main(argv):
+    options = Options()
     parser = argparse.ArgumentParser()
     parser.add_argument("--generated-text-file-path", type=str, required=True)
     parser.add_argument("--language-model-path", type=str, required=True)
-    args_namespace = parser.parse_args(argv)
-    command_line_args = vars(args_namespace)
+    parser.add_argument("--use-kenlm", action="store_true", default=False)
+
+    parser.parse_known_args(args=argv, namespace=options)
 
     global logger
     logger = log_initializer.setup_custom_logger(global_config.logger_name, "INFO")
 
-    ll_score = \
-        score_generated_sentences(command_line_args['generated_text_file_path'],
-                                  command_line_args['language_model_path'])
+    ll_score = score_generated_sentences(
+        options.generated_text_file_path, options.language_model_path, options.use_kenlm)
     logger.info("ll_score: {}".format(ll_score))
 
 
